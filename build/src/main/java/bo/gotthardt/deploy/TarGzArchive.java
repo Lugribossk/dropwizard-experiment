7bca8d49e258e4725dd80d8823ba3880c00ecf11
Bo Gotthardt <mail@gotthardt.bo>
2014-06-26 00:23:01 +0200

    Download JRE automatically.

--- build/src/main/java/bo/gotthardt/deploy/TarGzArchive.java
+++ build/src/main/java/bo/gotthardt/deploy/TarGzArchive.java
@@ -14,6 +14,7 @@ import java.util.Set;
 public class TarGzArchive {
     /**
      * Unpack a .tar.gz archive and return the folder it was unpacked in.
+     *
      * @param tarGz The archive
      * @return The unpacked folder
      */

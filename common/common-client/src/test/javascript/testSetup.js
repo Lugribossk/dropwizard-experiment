2c4609f11352899973c0facaa766873c9d346428
Bo Gotthardt <mail@gotthardt.bo>
2015-03-12 00:18:03 +0100

    Updated dependency versions.

--- common/common-client/src/test/javascript/testSetup.js
+++ common/common-client/src/test/javascript/testSetup.js
@@ -1,7 +1,7 @@
 var jsdom = require("jsdom");
 
 global.document = jsdom.jsdom("<!doctype html><html><body></body></html>");
-global.window = document.parentWindow;
+global.window = document._global;
 global.navigator = {
     userAgent: ""
 };

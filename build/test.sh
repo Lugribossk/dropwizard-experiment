242681585830c74c7c8dc49809a2cc3e4533b235
Bo Gotthardt <mail@gotthardt.bo>
2015-06-20 22:24:31 +0200

    Testing automatic redeploy.

--- build/test.sh
+++ build/test.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-curl -X POST \
--d '{"test": "test"}' \
-localhost
+set -x
 
-TEST=test
-curl -X POST \
--d '{"test": "'"${TEST}"'"}' \
-localhost
+github () {
+    URL=$1 # URL after repo name to post to.
+    PAYLOAD=$2 # JSON payload as a string.
+echo ${PAYLOAD}
+	curl -X POST -sS \
+	-H "Accept: application/vnd.github.v3+json" \
+	-H "Authorization: token "${GITHUB_TOKEN} \
+	-H "Content-Type: application/json" \
+	-H "User-Agent: Lugribossk deploy script" \
+	-d ${PAYLOAD} \
+	https://api.github.com/repos/${REPO_NAME}/${URL}
+}
+
+github deployments '{"ref": "'"${CIRCLE_SHA1}"'", "description": "CircleCI", "required_contexts": [], "environment": "test"}'
